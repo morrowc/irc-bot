f41c11ccef6c73d9c3653e8e728cf62696548cce
Chris Morrow <morrowc@ops-netman.net>
2026-02-17 02:03:40 -0500

    Fix latent error

--- run_client.sh
+++ run_client.sh
@@ -3,7 +3,7 @@
 go mod tidy
 
 # Run the client
-go run client/main.go client/tui.go
+# go run client/main.go client/tui.go
 # Note: tui.go doesn't exist separately, I put everything in main.go. 
 # So just:
 go run client/main.go
